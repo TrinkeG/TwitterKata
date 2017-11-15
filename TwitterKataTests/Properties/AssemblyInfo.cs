143f3371f07748eb7b4378a306025d66bc194af5
Christina Ripley-Gonzalez <christina.ripley@codurance.com>
2017-11-15 14:51:44 +0000

    initial

---
+++ TwitterKataTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TwitterKataTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TwitterKataTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("27784586-3347-4121-b41d-72acbc4d6faf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TwitterKataTests/TwitterKataPostFeature.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace TwitterKataTests
+{
+    [TestFixture]
+    public class TwitterKataPostFeature
+    {
+        
+    }
+}
